5d1a5444eae1cafa3707da47311269e626378580
fy9311 <455245435@qq.com>
2020-06-02 18:07:55 +0800

    init tracker.js

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -1,5 +1,6 @@
 import getLastEvent from '../utils/getLastEvent.js';
-import {getSelector} from '../utils/getSelector.js'
+import {getSelector} from '../utils/getSelector.js';
+import tracker from '../utils/tracker.js';
 
 export function injectJsError() {
     // 监听全局未捕获错误
@@ -10,14 +11,14 @@ export function injectJsError() {
             kind: 'stability', // 稳定性
             type: 'error', // 问题类型
             errorType: 'jsError', // 错误类型
-            url: event, // 报错路径
             message: event.message, // 报错信息
             filename: event.filename, // 报错文件
             position: `${event.lineno}:${event.colno}`, // 报错位置
             stack: event.error.stack, // 错误堆栈信息
-            selector: lastEvent ? getSelector(lastEvent.path) : null// 最后一个交互元素
+            selector: lastEvent ? getSelector(lastEvent.path) : null, // 最后一个交互元素
         }
-        console.log(errData)
+        // 上报数据
+        tracker.send(errData);
     })
 }
 
